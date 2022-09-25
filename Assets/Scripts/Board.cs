1bb2773b468d5d8251c90d921ed7221635bdc94d
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2022-09-24 23:28:34 -0300

    gave tilePrefab variable to board
    made tilePrefab serializable and loaded the tile game object prefab onto it
    
    deleted Tile game object from scene since tiles are going to be created programmatically

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    [SerializeField] private Tile tilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
