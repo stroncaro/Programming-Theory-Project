cc0ff766f0ff7c2d4912405e104a41f0320bb3cb
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2022-09-25 01:26:41 -0300

    make active or inactive tiles show accordingly
    implemented bordesCut variable to keep track of how many rows and columns of the board are out of bounds of the game area

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,9 +7,8 @@ public class Board : MonoBehaviour
     [SerializeField] private Tile tileActivePrefab;
     [SerializeField] private Tile tileInactivePrefab;
 
-
-    //make visible in inspector temporarily
-    private Tile[,] tileMap  = new Tile[9, 9];
+    private Tile[,] tileMap  = new Tile[10, 10];
+    private float bordersCut = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +18,14 @@ public class Board : MonoBehaviour
         {
             for (int y = 0; y < tileMap.GetLength(1); y++)
             {
-                Tile newTile = Instantiate(tileActivePrefab);
+                var tileCheck =
+                    x < bordersCut ||
+                    y < bordersCut ||
+                    x > tileMap.GetLength(0) - bordersCut -1 ||
+                    y > tileMap.GetLength(1) - bordersCut -1;
+                var prefab = tileCheck ? tileInactivePrefab : tileActivePrefab;
+
+                Tile newTile = Instantiate(prefab);
                 newTile.transform.position = new Vector2(x, y);
                 newTile.transform.SetParent(this.transform);
                 newTile.gameObject.name = string.Format("Tile({0},{1})", x, y);
