66f561261be466ffae00d41a6040259532599bf3
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2022-09-25 15:52:33 -0300

    create GameData singleton class

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public static GameData Instance;
+
+    [SerializeField] private Vector2 _boardSize;
+    public Board board { get; private set; }
+
+    void Initialize()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("There can only be one GameData instance");
+            return;
+        }
+
+        board = new Board((int)_boardSize.x, (int)_boardSize.y);
+    }
+
+    void Awake() => Initialize();
+}
