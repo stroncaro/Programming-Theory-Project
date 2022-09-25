d7905b6f7eb105cadc5b90d78e2d3aa7aa7620f8
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2022-09-24 23:52:43 -0300

    create tiles on board Start()
    create a 9x9 array tileMap to store board tiles and fill it on Start(), setting Board as parent of the tiles
    
    also create variables for the tile colors (unused yet)

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,15 +6,26 @@ public class Board : MonoBehaviour
 {
     [SerializeField] private Tile tilePrefab;
 
+    //colors for tiles, should be moved out later into a UI class or something
+    [SerializeField] private Color activeLightColor;
+    [SerializeField] private Color activeDarkColor;
+    [SerializeField] private Color inactiveLightColor;
+    [SerializeField] private Color inactiveDarkColor;
+
+    //make visible in inspector temporarily
+    [SerializeField] private Tile[,] tileMap  = new Tile[9, 9];
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int x = 0; x < tileMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < tileMap.GetLength(1); y++)
+            {
+                Tile newTile = Instantiate(tilePrefab);
+                newTile.transform.SetParent(this.transform);
+                tileMap[x,y] = newTile;
+            }
+        }
     }
 }
