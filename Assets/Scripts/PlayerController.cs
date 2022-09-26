defab3c907181f41ac3d61c1542df39134d92348
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2022-09-26 12:01:07 -0300

    create basic testing player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Avatar avatar;
+    [SerializeField] private Vector2 startPos;
+    [SerializeField] private Board.Direction startDirection;
+
     void Start()
     {
-        
+        //create an avatar
+        avatar = new Avatar("player", (int)startPos.x, (int)startPos.y, startDirection);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) { avatar.RotateCounterclockwise(); }
+            if (Input.GetKeyDown(KeyCode.R)) { avatar.RotateClockwise(); }
+        }
     }
 }
